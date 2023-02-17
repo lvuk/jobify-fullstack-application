8e3782fefeab93d890e3fefa843fd92a5e9fc813
lvuk <luka.vuk456@gmail.com>
2023-02-17 23:06:44 +0100

    finished landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,33 +1,36 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <img src={logo} alt='jobify' />
-      </nav>
-      <div className='container page'>
-        {/* info */}
-        <div className='info'>
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>
-            I'm baby polaroid palo santo succulents, actually schlitz biodiesel
-            tilde direct trade tousled literally cred. Palo santo squid
-            gluten-free ascot. Mukbang PBR&B lumbersexual tumeric viral enamel
-            pin vape hexagon big mood echo park af affogato. Glossier artisan
-            blog twee lo-fi echo park tacos humblebrag air plant yr keffiyeh
-            biodiesel disrupt. Authentic yes plz shoreditch lumbersexual
-            letterpress green juice artisan church-key gentrify enamel pin
-            yuccie.
-          </p>
-          <button className='btn btn-hero'>Login/Register</button>
+    <Wrapper>
+      <main>
+        <nav>
+          <img src={logo} alt='jobify' />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              I'm baby polaroid palo santo succulents, actually schlitz
+              biodiesel tilde direct trade tousled literally cred. Palo santo
+              squid gluten-free ascot. Mukbang PBR&B lumbersexual tumeric viral
+              enamel pin vape hexagon big mood echo park af affogato. Glossier
+              artisan blog twee lo-fi echo park tacos humblebrag air plant yr
+              keffiyeh biodiesel disrupt. Authentic yes plz shoreditch
+              lumbersexual letterpress green juice artisan church-key gentrify
+              enamel pin yuccie.
+            </p>
+            <button className='btn btn-hero'>Login/Register</button>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
-      </div>
-    </main>
+      </main>
+    </Wrapper>
   );
 };
 export default Landing;
