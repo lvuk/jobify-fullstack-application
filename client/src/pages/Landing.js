28cab44a8e83f0f86a1a95b7f1f0d88b9a6a1ad2
lvuk <luka.vuk456@gmail.com>
2023-02-17 22:41:38 +0100

    added normalize.css

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,7 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/logo.svg';
+
+const Landing = () => {
+  return <div>Landing page</div>;
+};
+export default Landing;
