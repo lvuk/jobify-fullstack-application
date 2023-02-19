3d9666eab35d957fc94494bd2b56c2dde2488def
lvuk <luka.vuk456@gmail.com>
2023-02-19 14:43:02 +0100

    added hashing password feature

--- api/middleware/error-handler.js
+++ api/middleware/error-handler.js
@@ -1,7 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorMiddleware = (err, req, res, next) => {
-  console.log(err.message);
+  console.log(err);
+
   const defaultError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, try again later',
