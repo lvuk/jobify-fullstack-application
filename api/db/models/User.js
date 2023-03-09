3275dbe66fcad0a330d29fbd58be33347e91eab7
lvuk <luka.vuk456@gmail.com>
2023-03-09 13:30:16 +0100

    finished login controller

--- api/db/models/User.js
+++ api/db/models/User.js
@@ -24,6 +24,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minLength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -48,6 +49,11 @@ UserSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
 };
 
 export default mongoose.model('User', UserSchema);
